4c0e7f02215e2e04ff151a41895d7f22351f9983
Trung Trinh <tqtrung83@gmail.com>
2021-02-04 00:34:35 -0330

    Added Launch Type

--- schema.js
+++ schema.js
@@ -8,11 +8,23 @@ const {
   GraphQLBoolean,
 } = require('graphql');
 
+//API documentation:  https://docs.spacexdata.com/
+
 //Create TYPE
 
 //Launch Type
 
-// https://docs.spacexdata.com/
+const LaunchType = new GraphQLObjectType({
+  name: 'Launch',
+  fields: {
+    flight_number: { type: GraphQLInt },
+    mission_name: { type: GraphQLString },
+    launch_year: { type: GraphQLString },
+    launch_date_local: { type: GraphQLString },
+    launch_success: { type: GraphQLBoolean },
+    rocket: { type: RocketType },
+  },
+});
 
 //Root Query
 const RootQuery = new GraphQLObjectType({});
