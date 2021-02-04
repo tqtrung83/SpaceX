e36c6f7c8cd12b825331ad871bf8bdeac8513bd4
Trung Trinh <tqtrung83@gmail.com>
2021-02-04 00:45:52 -0330

    Added Root Query

--- schema.js
+++ schema.js
@@ -7,6 +7,7 @@ const {
   GraphQLObjectType,
   GraphQLBoolean,
 } = require('graphql');
+const axios = require('axios');
 
 //API documentation:  https://docs.spacexdata.com/
 
@@ -37,7 +38,31 @@ const LaunchType = new GraphQLObjectType({
 });
 
 //Root Query
-const RootQuery = new GraphQLObjectType({});
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    launches: {
+      type: new GraphQLList(LaunchType),
+      args: {},
+      resolve(parent, args) {
+        return axios
+          .get('https://api.spacexdata.com/v3/launches')
+          .then((res) => res.data);
+      },
+    },
+    launch: {
+      type: LaunchType,
+      args: {
+        flight_number: { type: new GraphQLNonNull(GraphQLInt) },
+      },
+      resolve(parent, args) {
+        return axios
+          .get(`https://api.spacexdata.com/v3/launches/${args.flight_number}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
 
 module.exports = new GraphQLSchema({
   query: RootQuery,
