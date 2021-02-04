5c14da255281aa239451153d5ab9be2624d30954
Trung Trinh <tqtrung83@gmail.com>
2021-02-04 12:02:26 -0330

    Added Apollo Provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,24 @@
 import './App.css';
 import logo from './logo.png';
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <div className='App'>
-      <img
-        src={logo}
-        alt='SpaceX'
-        style={{ width: 200, display: 'bloc', margin: 'auto' }}
-      />
-    </div>
+    <ApolloProvider client={client}>
+      <div className='container'>
+        <img
+          src={logo}
+          alt='SpaceX'
+          style={{ width: 200, display: 'block', margin: 'auto' }}
+        />
+      </div>
+    </ApolloProvider>
   );
 }
 
