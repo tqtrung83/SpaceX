7bcba941844f61940c66838f6fcc950f8697ed6c
Trung Trinh <tqtrung83@gmail.com>
2021-02-04 02:07:03 -0330

    Added Client site

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import logo from './logo.png';
+
+function App() {
+  return (
+    <div className='App'>
+      <img
+        src={logo}
+        alt='SpaceX'
+        style={{ width: 200, display: 'bloc', margin: 'auto' }}
+      />
+    </div>
+  );
+}
+
+export default App;
